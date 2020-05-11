4a04138d15bf9e49aa8e163f7a84f6e037147b95
mosesap <mosesap20@gmail.com>
2020-05-11 12:17:52 -0400

    adding email reset functionality

--- budget_me/settings.py
+++ budget_me/settings.py
@@ -129,3 +129,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'forum-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+#TODO Lookup how to use environment variables
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+#this is your email addresss
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
