1c2e8f832a67458ad126342075fabee207cd65b5
mosesap <mosesap20@gmail.com>
2020-05-11 12:49:45 -0400

    added email settings

--- budget_me/settings.py
+++ budget_me/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from credentials import email, password
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,7 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-#TODO Lookup how to use environment variables
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-#this is your email addresss
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = email
+EMAIL_HOST_PASSWORD = password
